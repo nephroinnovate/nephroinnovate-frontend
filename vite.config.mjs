e149d851880a879ad0d9805a0122c3322ed053db
Aleksa Milic <aleksamilic2224@gmail.com>
2025-03-07 18:43:34 +0100

    changes for deployment on the static web app

--- vite.config.mjs
+++ vite.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         },
         build: {
             chunkSizeWarningLimit: 1600,
+            outDir: 'build',
             rollupOptions: {
                 output: {
                     manualChunks: {
